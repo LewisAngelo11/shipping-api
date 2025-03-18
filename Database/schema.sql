060e5db28dc34e4c813d7eee14058a0aa3ac0f2d
Luis Angel Soto Galvez <152150198+LewisAngelo11@users.noreply.github.com>
2025-03-18 00:39:28 -0700

    Creación del esquema de la BD
    Solo son los metadatos de la base de datos, NO datos.

---
+++ Database/schema.sql
@@ -0,0 +1,306 @@
+CREATE DATABASE  IF NOT EXISTS `paqueteria` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `paqueteria`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: paqueteria
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `codigos_postales`
+--
+
+DROP TABLE IF EXISTS `codigos_postales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `codigos_postales` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `CP` varchar(5) DEFAULT NULL,
+  `Asentamiento` varchar(45) DEFAULT NULL,
+  `Tipo_Asentamiento` varchar(40) DEFAULT NULL,
+  `C_Estado` int DEFAULT NULL,
+  `C_Mun` int DEFAULT NULL,
+  `D_CP` varchar(45) DEFAULT NULL,
+  `C_Oficina` varchar(45) DEFAULT NULL,
+  `C_Tipo_Asen` varchar(45) DEFAULT NULL,
+  `id_asenta_cpcons` varchar(45) DEFAULT NULL,
+  `d_zona` varchar(45) DEFAULT NULL,
+  `c_cve_ciudad` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_municipio_CP_idx` (`C_Estado`,`C_Mun`),
+  CONSTRAINT `fk_municipio_CP` FOREIGN KEY (`C_Estado`, `C_Mun`) REFERENCES `municipio` (`Id_EntidadFed`, `Id_Municipio`)
+) ENGINE=InnoDB AUTO_INCREMENT=155811 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `empleado`
+--
+
+DROP TABLE IF EXISTS `empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleado` (
+  `id_Empleado` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) DEFAULT NULL,
+  `Apellido1` varchar(30) DEFAULT NULL,
+  `Apellido2` varchar(30) DEFAULT NULL,
+  `Usuario` varchar(50) DEFAULT NULL,
+  `Contrasena` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_Empleado`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `entidad_federativa`
+--
+
+DROP TABLE IF EXISTS `entidad_federativa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entidad_federativa` (
+  `Id_EntidadFed` int NOT NULL,
+  `Nombre` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Nombre_ABRV` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Pob_Total` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `POB_MASCULINA` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `POB_FEMENINA` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `TOTAL DE VIVIENDAS` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`Id_EntidadFed`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `envio`
+--
+
+DROP TABLE IF EXISTS `envio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `envio` (
+  `id_Envio` int NOT NULL AUTO_INCREMENT,
+  `Fecha_Envio` date DEFAULT NULL,
+  `Fecha_Entrega` date DEFAULT NULL,
+  `Costo` double DEFAULT NULL,
+  `Origen` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Destino` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_Paquete` int DEFAULT NULL,
+  `id_Remitente` int DEFAULT NULL,
+  `id_Destinatario` int DEFAULT NULL,
+  `Estatus` enum('EN PROCESO','ENVIADO','EN ENTREGA A DOMICILIO','ENTREGADO') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id_Envio`),
+  KEY `id_Paquete` (`id_Paquete`),
+  KEY `id_Remitente` (`id_Remitente`),
+  KEY `id_Destinatario` (`id_Destinatario`),
+  CONSTRAINT `envio_ibfk_1` FOREIGN KEY (`id_Paquete`) REFERENCES `paquete` (`id_Paquete`),
+  CONSTRAINT `envio_ibfk_2` FOREIGN KEY (`id_Remitente`) REFERENCES `usuario` (`id_Usuario`),
+  CONSTRAINT `envio_ibfk_3` FOREIGN KEY (`id_Destinatario`) REFERENCES `usuario` (`id_Usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb3 */ ;
+/*!50003 SET character_set_results = utf8mb3 */ ;
+/*!50003 SET collation_connection  = utf8mb3_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `GetFecha_Actual` BEFORE INSERT ON `envio` FOR EACH ROW BEGIN
+    SET NEW.Fecha_Envio = NOW();
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `historial_ubicacion`
+--
+
+DROP TABLE IF EXISTS `historial_ubicacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial_ubicacion` (
+  `id_Paquete` int DEFAULT NULL,
+  `id_Envio` int DEFAULT NULL,
+  `id_Ubicacion` int DEFAULT NULL,
+  `Fecha_Actualizacion` date DEFAULT NULL,
+  KEY `id_Paquete` (`id_Paquete`),
+  KEY `id_Ubicacion` (`id_Ubicacion`),
+  CONSTRAINT `historial_ubicacion_ibfk_1` FOREIGN KEY (`id_Paquete`) REFERENCES `paquete` (`id_Paquete`),
+  CONSTRAINT `historial_ubicacion_ibfk_3` FOREIGN KEY (`id_Ubicacion`) REFERENCES `ubicacion` (`id_Ubicacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `localidad`
+--
+
+DROP TABLE IF EXISTS `localidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `localidad` (
+  `Id_EntidadFed` int NOT NULL,
+  `Id_Municipio` int NOT NULL,
+  `Id_Localidad` int NOT NULL,
+  `NOM_LOC` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `AMBITO` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `LATITUD` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `LONGITUD` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `LAT_DECIMAL` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `LON_DECIMAL` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ALTITUD` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `CVE_CARTA` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `POB_TOTAL` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `POB_MASCULINA` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `POB_FEMENINA` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `TOTAL_DE_VIVIENDAS_HABITADAS` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`Id_EntidadFed`,`Id_Municipio`,`Id_Localidad`),
+  CONSTRAINT `fk_MunLdad` FOREIGN KEY (`Id_EntidadFed`, `Id_Municipio`) REFERENCES `municipio` (`Id_EntidadFed`, `Id_Municipio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `municipio`
+--
+
+DROP TABLE IF EXISTS `municipio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `municipio` (
+  `Id_EntidadFed` int NOT NULL,
+  `Id_Municipio` int NOT NULL,
+  `Nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CVE_CAB` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `POB_TOTAL` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `POB_MASCULINA` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `POB_FEMENINA` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `TOTAL_DE_VIVIENDAS_HABITADAS` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`Id_EntidadFed`,`Id_Municipio`),
+  CONSTRAINT `EntFed_Municipio` FOREIGN KEY (`Id_EntidadFed`) REFERENCES `entidad_federativa` (`Id_EntidadFed`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `paquete`
+--
+
+DROP TABLE IF EXISTS `paquete`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paquete` (
+  `id_Paquete` int NOT NULL AUTO_INCREMENT,
+  `Peso` decimal(16,2) DEFAULT NULL,
+  `Estatus` enum('EN PROCESO','ENVIADO','EN ENTREGA A DOMICILIO','ENTREGADO') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Largo` decimal(16,2) DEFAULT NULL,
+  `Alto` decimal(16,2) DEFAULT NULL,
+  `Ancho` decimal(16,2) DEFAULT NULL,
+  PRIMARY KEY (`id_Paquete`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `rastreo`
+--
+
+DROP TABLE IF EXISTS `rastreo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rastreo` (
+  `Codigo_Rastreo` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_Paquete` int DEFAULT NULL,
+  `id_Envio` int DEFAULT NULL,
+  KEY `id_Paquete` (`id_Paquete`),
+  KEY `id_Envio` (`id_Envio`),
+  CONSTRAINT `rastreo_ibfk_1` FOREIGN KEY (`id_Paquete`) REFERENCES `paquete` (`id_Paquete`),
+  CONSTRAINT `rastreo_ibfk_2` FOREIGN KEY (`id_Envio`) REFERENCES `envio` (`id_Envio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ubicacion`
+--
+
+DROP TABLE IF EXISTS `ubicacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ubicacion` (
+  `id_Ubicacion` int NOT NULL,
+  `Descripcion` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id_Ubicacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id_Usuario` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Apellido1` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Apellido2` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL,
+  `Edad` int DEFAULT NULL,
+  `Email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Usuario` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Contrasena` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id_Usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `calcular_edad` BEFORE INSERT ON `usuario` FOR EACH ROW BEGIN
+    IF NEW.Fecha_Nacimiento IS NOT NULL THEN
+        SET NEW.Edad = YEAR(CURDATE()) - YEAR(NEW.Fecha_Nacimiento) -
+                       (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(NEW.Fecha_Nacimiento, '%m%d'));
+    ELSE
+        SET NEW.Edad = NULL;
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping events for database 'paqueteria'
+--
+
+--
+-- Dumping routines for database 'paqueteria'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-18  0:35:45
