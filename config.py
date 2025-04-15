5a3401c019a171e6c9ccdf703606714524d69383
Luis Angel Soto Galvez <152150198+LewisAngelo11@users.noreply.github.com>
2025-04-14 17:20:04 -0700

    Se ajustó un comentario sobre el token

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ DB_CONFIG = {
     'database': 'tu_base_datos'
 }
 
-# Clave secreta para JWT (Aún no integrado el token)
+# Clave secreta para JWT (Integrado y funcional pero no definitivo)
 JWT_SECRET = os.getenv("JWT_SECRET", "j35Fh$99!ad8$#G6@kL#1Pz!232$#")
